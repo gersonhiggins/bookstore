55bc4e28262f8ea6f667e5a4d8f69e9a63ed0c13
gersonhiggins <gahg.10gerson@gmail.com>
2023-06-13 16:47:11 -0400

    Fixes

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook, removeBook } from './redux/bookSlice';
+import { addBook, removeBook } from '../redux/books/bookSlice';
 import InputBooks from './logic/bookform';
 import BookList from './logic/booklist';
 
