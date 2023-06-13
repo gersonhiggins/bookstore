074ffb1e6287d9ec646b984d98e70a51d365a566
gersonhiggins <gahg.10gerson@gmail.com>
2023-06-13 16:00:03 -0400

    requested changes

--- src/components/redux/categories/categoriesSlice.js
+++ src/components/redux/categories/categoriesSlice.js
@@ -12,6 +12,11 @@ export const categoriesSlice = createSlice({
       const filterCat = state.categories.filter((book) => book.cat === action.payload);
       filterCat.status = 'w';
     },
+    checkStatus: (state, action) => {
+      state.categories = action.payload === 'Under construction'
+        ? 'Under construction'
+        : state.categories;
+    },
   },
 });
 
--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import bookReducer from './bookSlice';
-import categoriesReducer from './categoriesSlice';
+import bookReducer from './books/bookSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
